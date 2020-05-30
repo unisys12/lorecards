9407cd2d883d87730cff6fd48aba9a719c8fb295
Phillip Jackson <unisys12@gmail.com>
2020-05-30 09:23:58 -0500

    prototype concent

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,23 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "LoreNetwork Lore Cards",
+  plugins: [
+    {
+      use: "gridsome-source-google-sheets",
+      options: {
+        sheetId: process.env.ELDERSCROLS_SHEET_ID,
+        apiKey: process.env.GOOGLE_API_KEY,
+        type: "ES_Lore", //Optional - default is googleSheet. Used for graphql queries.
+      },
+    },
+    {
+      use: "gridsome-source-google-sheets",
+      options: {
+        sheetId: process.env.HALO_SHEET_ID,
+        apiKey: process.env.GOOGLE_API_KEY,
+        type: "Halo_Lore", //Optional - default is googleSheet. Used for graphql queries.
+      },
+    },
+  ],
+};
---
+++ src/pages/Halo.vue
@@ -0,0 +1,68 @@
+<template>
+  <Layout>
+    <main>
+      <header>
+        <h1>Daily Halo Lore Entry</h1>
+      </header>
+      <section class="card">
+        <div class="card-header">
+          <h2>{{ $page.allHaloLore.edges[0].node.Event }}</h2>
+          <sub>
+            <b>Year: </b>
+            <span>{{ $page.allHaloLore.edges[0].node.Year }}</span>
+          </sub>
+          <sub>
+            <b>Month: </b>
+            <span>{{ $page.allHaloLore.edges[0].node.Month }}</span>
+          </sub>
+          <sub>
+            <b>Day: </b>
+            <span>{{ $page.allHaloLore.edges[0].node.Day }}</span>
+          </sub>
+        </div>
+        <article class="card-body">
+          <p>{{ $page.allHaloLore.edges[0].node.Notes }}</p>
+        </article>
+        <footer class="card-footer">
+          <h3>{{ $page.allHaloLore.edges[0].node.Source }}</h3>
+          <a v-bind:href="$page.allHaloLore.edges[0].node.Halopedia_Page">
+            <h3>
+              {{ $page.allHaloLore.edges[0].node.Halopedia_Page }}
+            </h3>
+          </a>
+        </footer>
+      </section>
+    </main>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  allHaloLore(filter: { 
+    Month: { eq: "May" } 
+  	Day: { eq: "29" }	
+  }
+  ) {
+    edges {
+      node {
+        id
+        Year
+        Month
+        Day
+        Event
+        Source
+        Halopedia_Page
+        Notes
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Daily Halo Entry",
+  },
+};
+</script>
